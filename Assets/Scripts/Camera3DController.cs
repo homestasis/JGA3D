b3a670402e207417603f96ac0eab51ad7db40d4f
kuwabaray <yukiya104@gmail.com>
2020-04-21 22:47:17 +0900

    4_21 change

---
+++ Assets/Scripts/Camera3DController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera3DController : MonoBehaviour
+{
+    [SerializeField] private float offsetX;
+    [SerializeField] private List<float> posY;
+    [SerializeField] private GameObject player;
+
+    private float maxX;
+    private float posZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxX = -100;
+        posZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float playerX = player.transform.position.x;
+       
+        if (playerX >= 0)
+        {
+            if(maxX < 0) { maxX = playerX; }
+            transform.position = new Vector3(maxX + offsetX, posY[0], posZ);
+            
+        }
+        else
+        {
+            maxX = -100;
+            transform.position = new Vector3(playerX + offsetX, posY[0], posZ);
+        }
+    }
+
+
+   
+
+}
---
+++ Assets/Scripts/SunLightController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class SunLightController : MonoBehaviour
+{
+    [SerializeField]private Material sky;
+    [SerializeField]private GrassController grass;
+    [SerializeField] private float delta;
+
+    private new Light light;
+    private float ex;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        light = GetComponent<Light>();
+        light.intensity = 1f;
+        sky.SetFloat("_Exposure", 1f);
+        ex = 1f;
+    }
+
+    public async void Darken()
+    {
+        while (true)
+        {
+            float inte = light.intensity;
+            light.intensity -= delta;
+
+            ex -= delta;
+            sky.SetFloat("_Exposure", ex);
+
+            if (inte <= 0.07f || ex <= 0.01f)
+            {
+                light.intensity = 0.07f;
+
+                ex = 0.01f;
+                sky.SetFloat("_Exposure", ex);
+
+
+                break;
+            }
+            await Task.Delay(50);    
+        }
+
+        grass.GrowGrass();
+
+
+    }
+
+
+   
+    public  void LightLighten()
+    {
+       while(true)
+        {
+            float inte = light.intensity;
+            light.intensity += delta;
+            if (inte >= 1f)
+            {
+                break;
+            }
+            Thread.Sleep(50);
+        }
+
+    }
+
+   
+
+    public void SkyLighten()
+    {
+        while(true)
+        {
+            ex += delta;
+            sky.SetFloat("_Exposure", ex);
+            if (ex >= 1f)
+            {
+                ex = 1f;
+                sky.SetFloat("_Exposure", ex);
+                break;
+            }
+            Thread.Sleep(50);
+        }
+    }
+
+
+    
+}
