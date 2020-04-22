79b6250a318141f178d1d1178907b742568299da
kuwabaray <yukiya104@gmail.com>
2020-04-22 10:24:21 +0900

    雨の音追加

---
+++ Assets/Scripts/Rain3DController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class Rain3DController : MonoBehaviour
+{
+
+    [SerializeField] private float delta;
+    private AudioSource audioSource;
+
+    
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0;
+    }
+
+    internal async void StartToRain()
+    {
+        if (audioSource.volume >= 0.5f) { return; }
+        while (true)
+        {
+            audioSource.volume += delta;
+            if (audioSource.volume >= 0.5f)
+            {
+                audioSource.volume = 0.5f;
+                return;
+            }
+
+            await Task.Delay(50);
+        }
+    }
+
+    internal async void StopToRain()
+    {
+        if(audioSource.volume <= 0) { return; }
+        while (true)
+        {
+            audioSource.volume -= delta;
+            if (audioSource.volume <= 0f)
+            {
+                audioSource.volume = 0f;
+                return;
+            }
+
+            await Task.Delay(50);
+        }
+    }
+}
