50a26e26f0ac815dadbab01decfd91b739ec6d0c
kuwabaray <yukiya104@gmail.com>
2020-04-22 09:16:27 +0900

    水の増加

---
+++ Assets/Scripts/WaterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class WaterController : MonoBehaviour
+{
+    [SerializeField] private float delta;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+
+    private float initX;
+    private float initZ;
+     
+    private void Start()
+    {
+        initX = transform.position.x;
+        initZ = transform.position.z;
+    }
+
+    public async void IncreaseWater()
+    {
+        if (transform.position.y > maxY) { return; }
+        float sum = minY;
+        while (true)
+        {
+            sum += delta;
+            transform.position = new Vector3(initX, sum, initZ);
+            if (sum >= maxY)
+            {
+                sum = maxY;
+                transform.position = new Vector3(initX, sum, initZ);
+                break;
+            }
+            await Task.Delay(50);
+        }
+    }
+}
