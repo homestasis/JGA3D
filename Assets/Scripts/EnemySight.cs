465cc06daf5fb97b8e66c3ceb2b9a3f265517fe0
kuwabaray <yukiya104@gmail.com>
2020-04-24 11:34:24 +0900

    いろいろ編集

---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        GameObject root = transform.root.gameObject;
+        anim = root.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        anim.SetBool("isFind", true);
+        //GameOver();
+    }
+}
---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    private Light fireLight;
+
+    private void Awake()
+    {
+        fireLight = GetComponent<Light>();
+    }
+
+    internal void PutOutFire()
+    {
+        fireLight.enabled = false;
+    }
+}
