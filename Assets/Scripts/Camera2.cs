7473fdfe06280165fb16355b017d5936bd6e6b9f
koteramen <yhefscrdby@i.softbank.jp>
2020-05-05 15:00:35 +0900

    途中

---
+++ Assets/Scripts/Camera2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2 : MonoBehaviour
+{
+    [SerializeField] private float offsetX;
+    [SerializeField] private List<float> posY;
+    [SerializeField] private GameObject player;
+
+    private float posZ;
+    private float maxX;
+    private float currentY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxX = -100;
+        posZ = transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerX = player.transform.position.x;
+        float playerY = player.transform.position.y;
+        
+
+        if (playerX >= 0)
+        {
+            if (maxX < 0) { maxX = playerX; }
+            transform.position = new Vector3(maxX + offsetX, posY[0], posZ);
+            //print("pos0");
+        }
+        {
+            transform.position = new Vector3(playerX + offsetX, playerY + 1, posZ);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyGardian.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class EnemyGardian : MonoBehaviour
+{
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private float startPosX = 0;
+    [SerializeField] private float nextPosX = 0;
+    [SerializeField] private float speed = 0;
+
+    private Rigidbody rb = null;
+    private Animator anim = null;
+    private bool isRun = false;
+    private float vec;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        vec = nextPosX - startPosX;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine("RunAway");
+        }
+    }
+
+    IEnumerator RunAway()
+    {
+        if (vec < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, -90, 0);
+            rb.velocity = new Vector3(-speed, 0, 0);
+            isRun = true;
+            anim.SetBool("run", isRun);
+            yield return new WaitForSeconds(3f);
+            Destroy(this.gameObject);
+    
+        }
+        else if (vec > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 90, 0);
+            rb.velocity = new Vector3(speed, 0, 0);
+            isRun = true;
+            anim.SetBool("run", isRun);
+            yield return new WaitForSeconds(3f);
+            Destroy(this.gameObject);
+        }
+        else Destroy(this.gameObject);
+    }
+}
