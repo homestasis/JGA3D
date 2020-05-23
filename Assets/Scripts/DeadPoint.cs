2de9e4c935e0ab5fb06464e1e416881a385c0b58
koteramen <yhefscrdby@i.softbank.jp>
2020-05-23 19:36:00 +0900

    暗転と死亡アニメーション

--- Assets/Scripts/DeadPoint.cs
+++ Assets/Scripts/DeadPoint.cs
@@ -7,12 +7,20 @@ public class DeadPoint : MonoBehaviour
     [Header("ステージコントローラー")] public StageController ctrl;
 
     private string playerTag = "Player";
+    private GameObject player;
+    private NewPlayerController p;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        p = player.GetComponent<NewPlayerController>();
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.tag == playerTag)
-        { 
-            ctrl.PlayerSetContinuePoint();
+        {
+            //ctrl.PlayerSetContinuePoint();
         }
     }
 }
