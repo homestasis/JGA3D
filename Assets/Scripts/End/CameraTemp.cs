960e96d02b0d879e52daacd6ceefca1901b5c34e
kuwabaray <yukiya104@gmail.com>
2020-05-29 16:25:18 +0900

    クリア画面(仮)とstage2Obちょいいじり

---
+++ Assets/Scripts/End/CameraTemp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTemp : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x +1, 9f, -10);
+    }
+}
---
+++ Assets/Scripts/End/PlayerTemp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTemp : MonoBehaviour
+{
+    public float delta; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + delta, transform.position.y, transform.position.z);
+    }
+}
